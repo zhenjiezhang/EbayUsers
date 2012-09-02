0ff0a373af7495d009e44f214c7cc85cbcf136da
Zhenjie Zhang <zhenjiezhang@gmail.com>
2012-09-02 01:15:29 -0700

    first update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EbayUsers::Application.config.secret_token = '91c2cf0a85ce0c0b629a19e62a5a9e132ea55efc3441631cf97ec24162f277b616786f5e93bce22cdd5007fb2c25d62c49c34acd96a7f027a9c8aadc87f56d37'
